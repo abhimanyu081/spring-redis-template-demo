ad67d52c06d960c527f1f2b46bb40bb8f7578a73
Abhimanyu <abhimanyu081@outlook.com>
2017-12-26 16:55:24 +0530

    First Commit

---
+++ src/main/java/com/redis/springredistemplatedemo/SpringRedisTemplateDemoApplication.java
@@ -0,0 +1,12 @@
+package com.redis.springredistemplatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisTemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisTemplateDemoApplication.class, args);
+	}
+}
